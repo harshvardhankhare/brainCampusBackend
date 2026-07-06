b9bab734ccf91cafc8c0c4a47fa78a2b4cfc8c4e
harshvardhankhare <khareharsh543@gmail.com>
2026-07-06 20:43:56 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/braincampus/BraincampusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.braincampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraincampusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraincampusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braincampus/BraincampusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braincampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraincampusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
